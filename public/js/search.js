4e6e30cfd76b53767335e41630a367e9c01836a0
ludotzp <ludovic.touzepeiffer@gmail.com>
2017-05-09 16:23:36 -0400

    Update search.js

--- public/js/search.js
+++ public/js/search.js
@@ -69,7 +69,7 @@ function doSearch() {
                                              
   resultdiv.empty();
   if (result.length == 0) {
-    resultdiv.append('<p class="">No results found.1</p>');
+    resultdiv.append('<p class="">No results found.2</p>');
   } else if (result.length == 1) {
     resultdiv.append('<p class="">Found '+result.length+' result</p>');
   } else {
